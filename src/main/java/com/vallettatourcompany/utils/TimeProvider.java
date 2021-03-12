f33480ac18ad5cfef66676879c096b2f9edc5bda
Mark Micallef <mark.micallef@um.edu.mt>
2021-03-12 14:52:37 +0100

    Triggering CI

--- src/main/java/com/vallettatourcompany/utils/TimeProvider.java
+++ src/main/java/com/vallettatourcompany/utils/TimeProvider.java
@@ -5,7 +5,6 @@ public interface TimeProvider {
     public static int MORNING = 0;
     public static int AFTERNOON = 1;
     public static int EVENING = 2;
-    public static int NIGHT = 3;
 
     public int getDaySegment();
 }
